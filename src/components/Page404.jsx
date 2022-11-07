96fc15c1f356f8451f33871c6ca61a435e3e70ad
Samuel Chima <schima05@gmail.com>
2022-11-07 12:15:08 +0000

    404 & loading

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom'
 export default function Page404 (){
     return (
-                <div className='error_bg'>
+        <div className='error_bg'>
             <Link to='/'>Go Back Home </Link>
         </div>
     )
